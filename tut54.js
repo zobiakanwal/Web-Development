07f3082214cb88943b45e7ffb1bc0e3754c752c7
Zobia Kanwal <xobiaaijaz@gmail.com>
2020-11-18 20:29:03 +0500

    updated

--- tut54.js
+++ tut54.js
@@ -1,1 +1,10 @@
 alert("Alert does not return anything");
+
+let age = prompt("What's your age?","0");
+
+if (age>18){
+    console.log("You are allowed to get married");
+}
+else{
+    console.log("You are not allowed to get married");
+}
