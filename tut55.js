49ea2f203bcbc48a9a06ad9d56315b12ddbbeebc
Zobia Kanwal <xobiaaijaz@gmail.com>
2020-12-02 17:52:12 +0500

    updated

--- tut55.js
+++ tut55.js
@@ -6,6 +6,11 @@ for(i=0;i<4;i++){
 }
 
 let names = ['Ali', 'Ahsan', 'Arman', 'Zobia'];
+
 names.forEach(function (element){
-    console.log(element);
+    console.log("using forEach loop ",element);
 })
+
+for (element of names){
+    console.log("using for of loop ",element);
+}
